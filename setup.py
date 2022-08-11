c3a7290e21671c7741b7f9ec17a6fe107d48ce1a
ericbsd <ericturgeon.bsd@gmail.com>
2022-08-11 07:53:25 -0300

    Adding first layout of initial setup for setup-station

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+# from glob import glob
+from setuptools import setup
+
+# import DistUtilsExtra.command.build_extra
+# import DistUtilsExtra.command.build_i18n
+# import DistUtilsExtra.command.clean_i18n
+
+# to update i18n .mo files (and merge .pot file into .po files) run on Linux:
+# ,,python setup.py build_i18n -m''
+
+# silence pyflakes, __VERSION__ is properly assigned below...
+__VERSION__ = '0.1'
+
+# for line in open('gbinstall', 'r').readlines():
+#     if (line.startswith('__VERSION__')):
+#         exec(line.strip())
+PROGRAM_VERSION = __VERSION__
+
+
+def datafilelist(installbase, sourcebase):
+    datafileList = []
+    for root, subFolders, files in os.walk(sourcebase):
+        fileList = []
+        for f in files:
+            fileList.append(os.path.join(root, f))
+        datafileList.append((root.replace(sourcebase, installbase), fileList))
+    return datafileList
+
+
+prefix = sys.prefix
+
+# data_files.extend(datafilelist('{prefix}/share/locale'.format(prefix=sys.prefix), 'build/mo'))
+
+# cmdclass ={
+#             "build" : DistUtilsExtra.command.build_extra.build_extra,
+#             "build_i18n" :  DistUtilsExtra.command.build_i18n.build_i18n,
+#             "clean": DistUtilsExtra.command.clean_i18n.clean_i18n,
+# }
+
+setup(
+    name="install-station",
+    version=PROGRAM_VERSION,
+    description="setup-station is use to setup GhostBSD after installation",
+    license='BSD',
+    author='Eric Turgeon',
+    url='https://github/GhostBSD/setup-station/',
+    package_dir={'': '.'},
+    install_requires=['setuptools'],
+    scripts=['setup-station']
+)
+# cmdclass = cmdclass,
